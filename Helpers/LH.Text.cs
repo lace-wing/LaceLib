ec5f86988e3e43e238c7fb3ef737550b7fc72aac
lyc-Lacewing <lycrlsu01@gmail.com>
2023-06-25 23:36:43 +0800

    some h&u

---
+++ Helpers/LH.Text.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace LaceLib.Helpers
+{
+    public static partial class LH
+    {
+        public static string TagColor(this string text, Color color)
+        {
+            text = text.ReplaceLineEndings("\n");
+            text = text.Insert(text.Length, "]");
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (text[i] != '\n')
+                {
+                    continue;
+                }
+                text = text.Insert(i + 1, $"[c/{color.Hex3()}:");
+                text = text.Insert(i, "]");
+            }
+            text = text.Insert(0, $"[c/{color.Hex3()}:");
+            return text;
+        }
+    }
+}
---
+++ Utils/LU.Color.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaceLib.Utils
+{
+    public static partial class LU
+    {
+        public static string ToRGBHex(this Color color)
+        {
+            return (color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2")).ToLower();
+        }
+        public static string ToRGBAHex(this Color color)
+        {
+            return (color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2") + color.A.ToString("X2")).ToLower();
+        }
+    }
+}
