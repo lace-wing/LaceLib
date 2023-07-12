9ae97ceebd429551a975683b778056af644d310e
lyc-Lacewing <lycrlsu01@gmail.com>
2023-07-12 13:28:55 +0800

    take input...

---
+++ Testing/Commands/TestCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaceLib.Utils;
+using Terraria;
+
+namespace LaceLib.Testing.Commands
+{
+    internal class TestCommand : LU.Command
+    {
+        public override string Name => "test";
+        public override LU.Option[] PossibleOptions => new LU.Option[] { new LU.Option("-period", false, "p", "d") };
+        public override void Run(ref object pipe)
+        {
+            Console.WriteLine("tested");
+        }
+    }
+}
--- Utils/LU.Command.cs
+++ Utils/LU.Command.cs
@@ -18,35 +18,92 @@ namespace LaceLib.Utils
         public const char OptionIndicator = '-';
         public const char CommandSeparator = ' ';
         public const char CommandPiper = '|';
-        public static string[] PATH = new string[] { "./bin/commands/" };
         public abstract class Command
         {
             public abstract string Name { get; }
             public virtual string ManPath => $"./man/{Name}.txt";
             public virtual bool HasMan => File.Exists(ManPath);
             public virtual string Man => File.ReadAllText(ManPath);
+            public virtual HashSet<Option> PossibleOptions { get; private set; }
+            public string[] InvalidOptions { get; private set; }
             public int ExitCode { get; private set; }
             public object? Value { get; private set; }
-            public virtual Option[] Options { get; private set; }
+            private Dictionary<Option, string> options = new Dictionary<Option, string>();
+            private string[] arguments = new string[0];
             public abstract void Run(ref object? pipe);
             protected void Update(int exitCode, object? value)
             {
                 ExitCode = exitCode;
                 Value = value;
             }
+            public void TakeInput(string[] input)
+            {
+                bool valid = true, waitingParam = false, preArg = true;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!waitingParam && !input[i].IsOption())
+                    {
+                        preArg = false;
+                    }
+                    if (!preArg)
+                    {
+                        arguments = arguments.Append(input[i]).ToArray();
+                        continue;
+                    }
+                    if (input[i].IsOption())
+                    {
+                        foreach (Option op in PossibleOptions)
+                        {
+                            if (!op.Matches(input[i]))
+                            {
+                                continue;
+                            }
+                            valid = true;
+                            waitingParam = op.TakeParam;
+                            options.TryAdd(op, string.Empty);
+                            break;
+                        }
+                        if (!valid)
+                        {
+                            InvalidOptions = InvalidOptions.Append(input[i]).ToArray();
+                        }
+                        continue;
+                    }
+                    if (waitingParam)
+                    {
+                        options[options.Keys.Last()] = input[i];
+                        waitingParam = false;
+                    }
+                }
+            }
         }
         public struct Option
         {
             public readonly string Name;
-            public readonly string[] Abversions;
-            public readonly int ParamNum;
-            public Option(string name, string[] abv, int paramNum)
+            public readonly string[] Abbreviation;
+            public readonly bool TakeParam;
+            public Option(string name, bool takeParam, params string[] abv)
             {
                 Name = name;
-                Abversions = abv;
-                ParamNum = paramNum;
+                Abbreviation = abv;
+                TakeParam = takeParam;
             }
             public bool IsValid => !string.IsNullOrEmpty(Name);
+            public bool Matches(string text)
+            {
+                if (text == OptionIndicator + Name)
+                {
+                    return true;
+                }
+                foreach (string abv in Abbreviation)
+                {
+                    if (text == OptionIndicator + abv)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
         public static bool IsOption(this string text)
         {
