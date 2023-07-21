7611e70e73ca8b0587664e3d64a54b5079f29964
lyc-Lacewing <lycrlsu01@gmail.com>
2023-07-21 17:26:44 +0800

    some parser and fixes

--- Testing/Commands/TestCommand.cs
+++ Testing/Commands/TestCommand.cs
@@ -11,7 +11,7 @@ namespace LaceLib.Testing.Commands
     internal class TestCommand : LU.Command
     {
         public override string Name => "test";
-        public override LU.Option[] PossibleOptions => new LU.Option[] { new LU.Option("-period", false, "p", "d") };
+        public override HashSet<Option> PossibleOptions => base.PossibleOptions;
         public override void Run(ref object pipe)
         {
             Console.WriteLine("tested");
--- Utils/LU.Command.cs
+++ Utils/LU.Command.cs
@@ -14,107 +14,214 @@ using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
 
 namespace LaceLib.Utils
 {
-    public static partial class LU
-    {
-        public const char OptionIndicator = '-';
-        public const char CommandSeparator = ' ';
-        public const char CommandPiper = '|';
-        public abstract class Command
-        {
-            public abstract string Name { get; }
-            public virtual string ManPath => $"./man/{Name}.txt";
-            public virtual bool HasMan => File.Exists(ManPath);
-            public virtual string Man => File.ReadAllText(ManPath);
-            public virtual HashSet<Option> PossibleOptions { get; private set; }
-            public string[] InvalidOptions { get; private set; }
-            public int ExitCode { get; private set; }
-            public object? Value { get; private set; }
-            private Dictionary<Option, string> options = new Dictionary<Option, string>();
-            private string[] arguments = new string[0];
-            public abstract void Run(ref object? pipe);
-            protected void Update(int exitCode, object? value)
-            {
-                ExitCode = exitCode;
-                Value = value;
-            }
-            public void TakeInput(string[] input)
-            {
-                bool valid = true, waitingParam = false, preArg = true;
-                for (int i = 0; i < input.Length; i++)
-                {
-                    if (!waitingParam && !input[i].IsOption())
+	public static partial class LU
+	{
+		public const char OptionAbbrevIndicactor = '-';
+		public const string OptionNameIdicator = "--";
+		public const char CommandSeparator = ' ';
+		public const char CommandPiper = '|';
+		public const char CommandOutputter = '>';
+		public abstract class Command
+		{
+			public struct Option
+			{
+				public readonly string Name;
+				public readonly string Description;
+				public readonly char[] Abbreviation;
+				public readonly bool TakeParam;
+				public Option(string name, string description, bool takeParam, params char[] abv)
+				{
+					Name = name;
+					Description = description;
+					Abbreviation = abv;
+					TakeParam = takeParam;
+				}
+				public bool IsValid => !string.IsNullOrEmpty(Name);
+			}
+
+			public class OptionNameDuplicateException : ArgumentException
+			{
+				public readonly string[] ConflictingOptions;
+				public readonly string ConflictingName;
+				public OptionNameDuplicateException(string name, params Type[] options)
+				{
+					ConflictingName = name;
+					List<string> types = new List<string>();
+					foreach (var type in options)
+					{
+						types.Add(type.ToString());
+					}
+					ConflictingOptions = types.ToArray();
+				}
+				public OptionNameDuplicateException(string name, params Option[] options)
+				{
+					ConflictingName = name;
+                    List<string> types = new List<string>();
+                    foreach (var op in options)
                     {
-                        preArg = false;
+                        types.Add(op.GetType().ToString());
                     }
-                    if (!preArg)
-                    {
-                        arguments = arguments.Append(input[i]).ToArray();
-                        continue;
-                    }
-                    if (input[i].IsOption())
-                    {
-                        foreach (Option op in PossibleOptions)
-                        {
-                            if (!op.Matches(input[i]))
-                            {
-                                continue;
-                            }
-                            valid = true;
-                            waitingParam = op.TakeParam;
-                            options.TryAdd(op, string.Empty);
-                            break;
-                        }
-                        if (!valid)
-                        {
-                            InvalidOptions = InvalidOptions.Append(input[i]).ToArray();
-                        }
-                        continue;
-                    }
-                    if (waitingParam)
-                    {
-                        options[options.Keys.Last()] = input[i];
-                        waitingParam = false;
-                    }
+                    ConflictingOptions = types.ToArray();
                 }
-            }
-        }
-        public struct Option
-        {
-            public readonly string Name;
-            public readonly string Description;
-            public readonly string[] Abbreviation;
-            public readonly bool TakeParam;
-            public Option(string name, string description, bool takeParam, params string[] abv)
-            {
-                Name = name;
-                Description = description;
-                Abbreviation = abv;
-                TakeParam = takeParam;
-            }
-            public bool IsValid => !string.IsNullOrEmpty(Name);
-            public bool Matches(string text)
-            {
-                if (text == OptionIndicator + Name)
-                {
-                    return true;
-                }
-                foreach (string abv in Abbreviation)
-                {
-                    if (text == OptionIndicator + abv)
+			}
+			public class OptionAbbrevDuplicateException : ArgumentException
+			{
+				public readonly string[] ConflictingOptions;
+				public readonly string ConflictingAbbrev;
+				public OptionAbbrevDuplicateException(string abbrev, params Type[] options)
+				{
+					ConflictingAbbrev = abbrev;
+                    List<string> types = new List<string>();
+                    foreach (var type in options)
                     {
-                        return true;
+                        types.Add(type.ToString());
                     }
+                    ConflictingOptions = types.ToArray();
                 }
-                return false;
-            }
-        }
-        public static bool IsOption(this string text)
-        {
-            if (string.IsNullOrEmpty(text) || text.Length < 1)
-            {
-                return false;
-            }
-            return text[1] == OptionIndicator;
-        }
-    }
+				public OptionAbbrevDuplicateException(string abbrev, params Option[] options)
+				{
+					ConflictingAbbrev = abbrev;
+                    List<string> types = new List<string>();
+                    foreach (var op in options)
+                    {
+                        types.Add(op.GetType().ToString());
+                    }
+                    ConflictingOptions = types.ToArray();
+                }
+			}
+			public class OptionInvalidException : ArgumentException
+			{
+				public readonly string OptionName;
+				public OptionInvalidException(string hypenedOptionName)
+				{
+					OptionName = hypenedOptionName;
+				}
+			}
+			public class OptionTooShortException : ArgumentException
+			{
+				public OptionTooShortException()
+				{
+				}
+			}
+			public class OptionArgumentNullException : ArgumentNullException
+			{
+				public readonly Option ErroredOption;
+				public OptionArgumentNullException(Option option)
+				{
+					ErroredOption = option;
+				}
+			}
+
+			public abstract string Name { get; }
+			public virtual string ManPath => $"./man/{Name}.txt";
+			public virtual bool HasMan => File.Exists(ManPath);
+			public virtual string Man => File.ReadAllText(ManPath);
+			public virtual HashSet<Option> PossibleOptions { get; private set; }
+			public string[] InvalidOptions { get; private set; }
+			public int ExitCode { get; private set; }
+			public object? Value { get; private set; }
+			private Dictionary<string, Dictionary<Option, string>> arguments = new Dictionary<string, Dictionary<Option, string>>();
+			public abstract void Run(ref object? pipe);
+			protected void Update(int exitCode, object? value)
+			{
+				ExitCode = exitCode;
+				Value = value;
+			}
+			protected bool AddOption(string arg, Option option, string param)
+			{
+				if (!arguments.ContainsKey(arg))
+				{
+					return false;
+				}
+				return arguments[arg].TryAdd(option, param);
+			}
+			protected bool AddArg(string arg)
+			{
+				return arguments.TryAdd(arg, new Dictionary<Option, string>());
+			}
+			protected Option[] MatchOptions(string text)
+			{
+				if (text.Length < 2 || !text.IsOption())
+				{
+					throw new OptionTooShortException();
+				}
+				List<Option> ops = new List<Option>();
+				bool isFullName = text.IsOptionFullName();
+                foreach (Option op in PossibleOptions)
+                {
+                    if (isFullName && op.Name == text[2..])
+                    {
+                        ops.Add(op);
+                        break;
+                    }
+					else
+					{
+						text = text[1..];
+						foreach (char o in text)
+						{
+							if (op.Abbreviation.Contains(o))
+							{
+								ops.Add(op);
+								continue;
+							}
+							throw new OptionInvalidException($"{OptionAbbrevIndicactor}{o}");
+						}
+					}
+                    throw new OptionInvalidException(text);
+                }
+				return ops.ToArray();
+			}
+			public void TakeInput(string[] input)
+			{
+				bool validOption = true, isOptionArg = false;
+				string lastArg = string.Empty;
+				Option lastOption = default;
+				for (int i = 0; i < input.Length; i++)
+				{
+					if (!isOptionArg && !input[i].IsOption())
+					{
+						lastArg = input[i];
+						AddArg(input[i]);
+						continue;
+					}
+					if (input[i].IsOption())
+					{
+						foreach (Option op in PossibleOptions)
+						{
+							if (!op.Matches(input[i]))
+							{
+								continue;
+							}
+							validOption = true;
+							isOptionArg = op.TakeParam;
+							options.TryAdd(op, string.Empty);
+							break;
+						}
+						if (!validOption)
+						{
+							InvalidOptions = InvalidOptions.Append(input[i]).ToArray();
+						}
+						continue;
+					}
+					if (isOptionArg)
+					{
+						options[options.Keys.Last()] = input[i];
+						isOptionArg = false;
+					}
+				}
+			}
+		}
+		public static bool IsOption(this string text)
+		{
+			if (string.IsNullOrEmpty(text) || text.Length < 1)
+			{
+				return false;
+			}
+			return text[1] == OptionIndicator;
+		}
+		public static bool IsOptionFullName(this string text)
+		{
+			return text.Length > 2 && text[0..1] == OptionNameIdicator;
+		}
+	}
 }
